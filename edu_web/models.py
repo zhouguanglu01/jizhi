33b8792e0ab205e6fbfe8e339694eee29e5fd239
zhouguanglu01 <zhouguanglu2012@163.com>
2019-08-05 10:50:10 +0800

    ddddddddddd

--- edu_web/models.py
+++ edu_web/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class  Footer(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.CharField(max_length=32, null=False)
+    link = models.URLField(max_length=255,null=False)
+    isdisplay = models.BooleanField(default=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+class SiteInfo(models.Model):
+    siteName = models.CharField(max_length=64,primary_key=True)
+    siteFlag = models.CharField(max_length=32)
+    isValid = models.BooleanField(default=True)
+    description = models.CharField(max_length=32)
+
